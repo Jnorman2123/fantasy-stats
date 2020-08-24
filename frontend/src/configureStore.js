14fb161dff47c32408433ad68890db32c79b1d82
Jnorman2123 <54901101+Jnorman2123@users.noreply.github.com>
2020-08-24 18:11:28 -0400

    created a react app frontend and added a configure store file

---
+++ frontend/src/configureStore.js
@@ -0,0 +1,18 @@
+import { createBrowserHistory } from "history";
+import { applyMiddleware, compose, createStore } from "redux";
+import { routerMiddleware } from "connected-react-router";
+import thunk from "redux-thunk";
+import rootReducer from "./reducers/rootReducer";
+
+export const history = createBrowserHistory();
+
+export default function configureStore(preloadedState) {
+  const composeEnhancer =
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  const store = createStore(
+    rootReducer(history),
+    preloadedState,
+    composeEnhancer(applyMiddleware(routerMiddleware(history), thunk))
+  );
+  return store;
+}
