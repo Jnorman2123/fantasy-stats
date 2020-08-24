77d4a65b6df73d3bffee1f2def2db8470cc8f144
Jnorman2123 <54901101+Jnorman2123@users.noreply.github.com>
2020-08-24 15:45:25 -0400

    added django rest framework to installed apps

--- backend/settings.py
+++ backend/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'players.apps.PlayersConfig'
+    'players.apps.PlayersConfig',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
