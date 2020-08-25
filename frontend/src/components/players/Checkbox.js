65bcd26f61d3f23a0c72983fe80b811d69f5a066
Jnorman2123 <54901101+Jnorman2123@users.noreply.github.com>
2020-08-25 14:35:03 -0400

    added checkbox component

---
+++ frontend/src/components/players/Checkbox.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+
+class Checkbox extends Component {
+  render() {
+    return (
+      <div className="form-check">
+        <label>
+          <input
+            type="checkbox"
+            name={this.props.stat}
+            toggled={this.props.isToggled}
+            onChange={this.props.handleToggle}
+            className="form-check-input"
+          />
+          {this.props.stat}
+        </label>
+      </div>
+    );
+  }
+}
+
+export default Checkbox;
